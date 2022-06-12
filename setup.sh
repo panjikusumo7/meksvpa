b8d7dbe4f92d2fbc59c9ecdd3c1979dacdc9e236
panjikusumo7 <103624384+panjikusumo7@users.noreply.github.com>
2022-06-12 23:12:40 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,20 @@
-#!binbash
+#!/bin/bash
 # ==========================================
 # Color
-RED='033[0;31m'
-NC='033[0m'
-GREEN='033[0;32m'
-ORANGE='033[0;33m'
-BLUE='033[0;34m'
-PURPLE='033[0;35m'
-CYAN='033[0;36m'
-LIGHT='033[0;37m'
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 # ==================================================
 # Link Hosting Kalian
-
-luminevpn=raw.githubusercontent.companjikusumo7meksvpamainssh
+luminevpn="raw.githubusercontent.com/panjikusumo7/meksvpa"
 
 # disable ipv6
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
@@ -32,6 +31,7 @@ apt-get remove --purge exim4 -y
 apt -y install wget curl
 
 # Install Requirements Tools
+apt install ruby -y
 apt install python -y
 apt install make -y
 apt install cmake -y
@@ -41,26 +41,51 @@ apt install net-tools -y
 apt install zip -y
 apt install unzip -y
 apt install nano -y
+apt install sed -y
+apt install gnupg -y
+apt install gnupg1 -y
+apt install bc -y
+apt install jq -y
+apt install apt-transport-https -y
+apt install build-essential -y
+apt install dirmngr -y
+apt install libxml-parser-perl -y
 apt install neofetch -y
+apt install git -y
+apt install lsof -y
+apt install libsqlite3-dev -y
+apt install libz-dev -y
+apt install gcc -y
+apt install g++ -y
+apt install libreadline-dev -y
+apt install zlib1g-dev -y
+apt install libssl-dev -y
+apt install libssl1.0-dev -y
+apt install dos2unix -y
 
 # set time GMT +7
-ln -fs usrsharezoneinfoAsiaJakarta etclocaltime
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# install
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+echo "clear" >> .profile
+echo "neofetch" >> .profile
 
 # download script
-cd usrbin
-wget -O menu https${luminevpn}meksvpamenu.sh
-wget -O clearlog https${luminevpn}clearlog.sh
-wget -O xp https${luminevpn}xp.sh
-wget -O addvmess https${luminevpn}addv2ray.sh
-wget -O addtrojan https${luminevpn}addtrojan.sh
-wget -O delvmess https${luminevpn}delv2ray.sh
-wget -O deltrojan https${luminevpn}deltrojan.sh
-wget -O cekvmess https${luminevpn}cekv2ray.sh
-wget -O cektrojan https${luminevpn}cektrojan.sh
-wget -O renewvmess https${luminevpn}renewv2ray.sh
-wget -O renewtrojan https${luminevpn}renewtrojan.sh
-wget -O vmessmenu https${luminevpn}meksvpavmessmenu.sh
-wget -O trmenu https${luminevpn}meksvpatrmenu.sh
+cd /usr/bin
+wget -O menu "https://${luminevpn}/menu.sh"
+wget -O clearlog "https://${luminevpn}/clearlog.sh"
+wget -O xp "https://${luminevpn}/xp.sh"
+wget -O addvmess "https://${luminevpn}/addv2ray.sh"
+wget -O addtrojan "https://${luminevpn}/addtrojan.sh"
+wget -O delvmess "https://${luminevpn}/delv2ray.sh"
+wget -O deltrojan "https://${luminevpn}/deltrojan.sh"
+wget -O cekvmess "https://${luminevpn}/cekv2ray.sh"
+wget -O cektrojan "https://${luminevpn}/cektrojan.sh"
+wget -O renewvmess "https://${luminevpn}/renewv2ray.sh"
+wget -O renewtrojan "https://${luminevpn}/renewtrojan.sh"
+wget -O vmessmenu "https://${luminevpn}/vmessmenu.sh" 
+wget -O trmenu "https://${luminevpn}/trmenu.sh"
 
 
 chmod +x vmessmenu
@@ -81,12 +106,20 @@ echo "0 0 * * * root xp" >> /etc/crontab
 # remove unnecessary files
 cd
 apt autoclean -y
+apt -y remove --purge unscd
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove bind9*;
+apt-get -y remove sendmail*
 apt autoremove -y
+# finishing
+cd
 history -c
 
 cd
-rm -f rootsetup.sh
+rm -f /root/setup.sh
 
 # finihsing
 clear
-echo Installation has been completed!!
+
+echo "Installation has been completed!!"
