95cf10b72458116476f393be9d60230fbfbe90d3
panjikusumo7 <103624384+panjikusumo7@users.noreply.github.com>
2022-06-16 16:58:35 +0700

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -12,6 +12,19 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
+data=( `cat /root/v2ray/config.json | grep '^#@#' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#@# $user" "/root/v2ray/config.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^#@# $user $exp/,/^},{/d" /root/v2ray/config.json
+fi
+done
+systemctl restart xray.service
 data=( `cat /root/v2ray/config.json | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
@@ -22,19 +35,6 @@ d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" = "0" ]]; then
 sed -i "/^### $user $exp/,/^},{/d" /root/v2ray/config.json
-fi
-done
-systemctl restart xray.service
-data=( `cat /root/v2ray/config.json | grep '^####' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^#### $user" "/root/v2ray/config.json" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^#### $user $exp/,/^},{/d" /root/v2ray/config.json
 rm -f /root/v2ray/vmess-$user-tls.json /root/v2ray/vmess-$user-cdn.json
 fi
 done
